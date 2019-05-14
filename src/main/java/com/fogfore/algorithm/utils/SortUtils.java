68c50549c790a766d2dc71118fd3e60608acb7b7
fogfore <38969741+fogfore@users.noreply.github.com>
2019-05-14 10:36:37 +0800

    Update SortUtils.java

--- src/main/java/com/fogfore/algorithm/utils/SortUtils.java
+++ src/main/java/com/fogfore/algorithm/utils/SortUtils.java
@@ -1,15 +1,64 @@
 package com.fogfore.algorithm.utils;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.Validate;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SortUtils {
     private static final ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
 
-    public static int[] quickSort(int[] array) {
-        return null;
+    public static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
     }
 
-    public static int[] randomQuickSort(int[] array) {
-        return null;
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int mid = partition(array, low, high, false);
+            quickSort(array, low, mid - 1);
+            quickSort(array, mid + 1, high);
+        }
+    }
+
+
+    public static void randomQuickSort(int[] array) {
+        randomQuickSort(array, 0, array.length - 1);
+    }
+
+    public static void randomQuickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int mid = partition(array, low, high, true);
+            randomQuickSort(array, low, mid - 1);
+            randomQuickSort(array, mid + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high, boolean isRandom) {
+        if (low > high) {
+            return -1;
+        }
+        if (isRandom) {
+            int index = RANDOM.nextInt(low, high + 1);
+            swap(array, index, high);
+        }
+        int small = low - 1;
+        int temp = array[high];
+        for (int i = low; i < high; i++) {
+            if (array[i] < temp) {
+                small++;
+                if (small != i) {
+                    swap(array, small, i);
+                }
+            }
+        }
+        small++;
+        swap(array, small, high);
+        return small;
+    }
+
+    public static void swap(int[] array, int origin, int dest) {
+        int temp = array[origin];
+        array[origin] = array[dest];
+        array[dest] = temp;
     }
 }
