f318dbfe801b063c59dda988d762e135f8631a76
fogfore <38969741+fogfore@users.noreply.github.com>
2019-05-14 10:37:44 +0800

    Update TreeUtils.java

--- src/main/java/com/fogfore/algorithm/utils/TreeUtils.java
+++ src/main/java/com/fogfore/algorithm/utils/TreeUtils.java
@@ -1,29 +1,8 @@
 package com.fogfore.algorithm.utils;
 
+
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TreeUtils {
-    public static final int DEFAULT_NODE_NUM = 50;
-    public static final int MAX_VALUE = 100;
 
-    private static final ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
-
-    public TreeNode generateTree() {
-        return generateTree(DEFAULT_NODE_NUM);
-    }
-
-    public TreeNode generateTree(int nodeNum) {
-        if (nodeNum < 1) {
-            return null;
-        }
-        nodeNum--;
-        TreeNode root = new TreeNode(RANDOM.nextInt(MAX_VALUE));
-        if (RANDOM.nextBoolean()) {
-            root.setLeft(generateTree(nodeNum));
-        }
-        if (RANDOM.nextBoolean()) {
-            root.setRight(generateTree(nodeNum));
-        }
-        return root;
-    }
 }
